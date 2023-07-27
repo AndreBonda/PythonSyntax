2ee008414d042912852640e45c1c1daf773481f3
Bonda <bondaniniandrea@gmail.com>
2023-07-27 11:44:14 +0200

    Add del statement

---
+++ del_statement.py
@@ -0,0 +1,21 @@
+# Del statement offers a way to delete item from a list given its index
+
+# delete single element
+l = [-2,-1,1,2,5,8,9]
+del l[0]
+print(l) # [-1, 1, 2, 5, 8, 9]
+
+# delete slice
+l = [-2,-1,1,2,5,8,9]
+del l[1:3] # elimina elemento in posizione 1 e 2
+print(l) # [-2, 2, 5, 8, 9]
+
+# delete all elements
+l = [-2,-1,1,2,5,8,9]
+del l[:]
+print(l) # []
+
+# delete a variable. You can't reference it after this instruction
+l = [-2,-1,1,2,5,8,9]
+del l
+# print(l) -> It throws 'NameError: name 'l' is not defined'
