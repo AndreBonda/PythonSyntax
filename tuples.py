79d1669032cfb9353f12ea75e1b4e9b6133ef31c
Bonda <bondaniniandrea@gmail.com>
2023-07-27 12:06:52 +0200

    Add tuples

--- tuples.py
+++ tuples.py
@@ -1,11 +1,30 @@
-# Tuples are like arrays but immutable
-tup = (1,2,3)
-print(tup)
-print(tup[0])
-print(tup[-1])
+# Tuples are sequence data type like strings and arrays. Tuples are immutables.
 
-# Can be used as key for hash map/set
-myMap = {(1,2): 3}
-myMap = {(3,4): 3}
-print(myMap)
-print((3,4) in myMap) # True
+# tuple packing
+t = 1995, "Andrea", "Bondanini" # oppure t = (val1, val2, ...)
+print(t)
+print(t[0])
+
+# nested tuples
+t = (5,"Giugno"), "Andrea"
+print(t)
+
+# tuples are immutable but can contain mutable objects
+t = ([1,2],[3,4])
+t[0][0] = 0
+print(t) # ([0, 2], [3, 4])
+
+# empty tuple
+t = ()
+print(t) # ()
+
+# tuple with 1 value.
+t = ("Hello",) # comma is needed
+print(t)
+
+# tuple unpacking
+t = 1995, "Andrea", "Bondanini"
+year, firstname, surname = t
+print(year)
+print(firstname)
+print(surname)
